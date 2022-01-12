9485243092efb5754f0f1f58eaf9c833d8cb576e
JongDo737 <jongmin373@gmail.com>
2022-01-12 20:44:26 +0900

    기본 파일

---
+++ team-project-president/src/main/java/com/springboot/president/TeamProjectPresidentApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.president;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectPresidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectPresidentApplication.class, args);
+	}
+
+}
