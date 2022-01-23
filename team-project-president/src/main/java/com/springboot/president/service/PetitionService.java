9588cdb8f769bc3826c9c3c1e151fbead7d23003
DESKTOP-D3H1U32\edgea <beanbeeean@naver.com>
2022-01-23 19:23:02 +0900

    petition_board merge 완료

--- team-project-president/src/main/java/com/springboot/president/service/PetitionService.java
+++ team-project-president/src/main/java/com/springboot/president/service/PetitionService.java
@@ -13,7 +13,6 @@ public interface PetitionService {
 	public GetPetitionRespDto GetPetitionByid(PrincipalDetails principalDetails);
 	public GetPetitionRespDto GetWaitPetition();
 	public GetPetitionRespDto GetPetitionBykategorieAndOrder(String kategorie,int only, int page, int order);
-	public GetPetitionRespDto GetPetitionByKategorie(String kategorie);
 	public GetPetitionRespDto GetPetitionByAgreeCount();
 	public BoardPetitionRespDto BoardPetitionByPetitionid(PrincipalDetails principalDetails, int petition_id);
 	public boolean insertPetitionReply(PrincipalDetails principalDetails, ReplyReqDto replyReqDto);
